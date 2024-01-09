0ab31aebd320a6155cb7f57920bc749aff554e1f
thonyETU1883 <thonyrasolofondraibe1883@gmail.com>
2024-01-09 20:35:15 +0100

    stat

--- src/main/java/com/voiture/voiture/VoitureAppApplication.java
+++ src/main/java/com/voiture/voiture/VoitureAppApplication.java
@@ -1,12 +1,21 @@
 package com.voiture.voiture;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.voiture.voiture.modele.VoitureDefiniStatView;
 
 @SpringBootApplication
 public class VoitureAppApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		VoitureDefiniStatView v = new VoitureDefiniStatView();
+		List<VoitureDefiniStatView> liste = v.getVentetolal(null, "2023-11-01", "2023-11-11");
+		for (VoitureDefiniStatView voitureDefiniStatView : liste) {
+			System.out.println(voitureDefiniStatView.getNomMarque());
+		}
 		SpringApplication.run(VoitureAppApplication.class, args);
 	}
 
--- src/main/java/com/voiture/voiture/repository/VoitureDefiniStatViewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.voiture.voiture.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.voiture.voiture.modele.VoitureDefiniStatView;
-
-public interface VoitureDefiniStatViewRepository extends JpaRepository<VoitureDefiniStatView,Integer>{
-    
-}
