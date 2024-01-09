12db914929edb2fa8c037bcd5f6bfea70d2895cb
Rasolofondraibe <raobijaonanoah3@gmail.com>
2024-01-09 21:00:52 +0100

    ok

--- src/main/java/com/voiture/voiture/controller/BoitedevitesseController.java
+++ src/main/java/com/voiture/voiture/controller/BoitedevitesseController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/Boitedevitesse")
 @AllArgsConstructor
+@CrossOrigin
 public class BoitedevitesseController {
     private final BoiteDeVitesseService boiteDeVitesseService;
 
