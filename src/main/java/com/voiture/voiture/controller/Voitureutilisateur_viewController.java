d223f39e53c968bda8a91d2cf6b17fe0f9fb5661
thonyETU1883 <thonyrasolofondraibe1883@gmail.com>
2024-01-17 00:45:21 +0100

    OK

--- src/main/java/com/voiture/voiture/controller/Voitureutilisateur_viewController.java
+++ src/main/java/com/voiture/voiture/controller/Voitureutilisateur_viewController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,11 @@ public class Voitureutilisateur_viewController {
     @GetMapping("/rechercheMultiple/{prixmin}/{prixmax}")
     public List<Voitureutilisateur_view> rechercheMultiple(@RequestBody Voitureutilisateur_view v,@PathVariable double prixmin,@PathVariable double prixmax) throws Exception{
         return v.rechercheMultiple(null,prixmin,prixmax);
+    }
+    
+    @PostMapping("/annonce/{idUtilisateur}")
+    public List<Voitureutilisateur_view> get_annonce_autre_utilisateur(@PathVariable int idUtilisateur) throws Exception{
+        Voitureutilisateur_view v = new Voitureutilisateur_view(); 
+        return v.get_annonce_autre_utilisateur(null,idUtilisateur);
     }
 }
