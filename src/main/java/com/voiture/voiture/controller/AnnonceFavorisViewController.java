88b884427926a6d518a681c8c99551d1ffbb768d
Rasolofondraibe <raobijaonanoah3@gmail.com>
2024-01-23 21:22:00 +0100

    ok

--- src/main/java/com/voiture/voiture/controller/AnnonceFavorisViewController.java
+++ src/main/java/com/voiture/voiture/controller/AnnonceFavorisViewController.java
@@ -1,19 +1,28 @@
 package com.voiture.voiture.controller;
 
 import com.voiture.voiture.modele.AnnonceFavorisView;
+import com.voiture.voiture.modele.MyToken;
 import com.voiture.voiture.service.AnnonceFavorisViewService;
+import com.voiture.voiture.service.MyTokenService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/AnnonceFavorisView")
 @CrossOrigin
 public class AnnonceFavorisViewController {
     private final AnnonceFavorisViewService annonceFavorisViewService;
+    private final MyTokenService myTokenService;
 
-    public AnnonceFavorisViewController(AnnonceFavorisViewService annonceFavorisViewService) {
+    public AnnonceFavorisViewController(AnnonceFavorisViewService annonceFavorisViewService,MyTokenService myTokenService) {
         this.annonceFavorisViewService = annonceFavorisViewService;
+        this.myTokenService =myTokenService;
     }
 
     @GetMapping("/read")
@@ -22,7 +31,13 @@ public class AnnonceFavorisViewController {
     }
 
     @GetMapping("/findAnnonceFavorisByIdUser/{idutilisateur}")
-    public List<AnnonceFavorisView> findAnnonceFavorisByIdUser(@PathVariable int idutilisateur){
-        return annonceFavorisViewService.findAnnonceFavorisByIdUser(idutilisateur);
+    public ResponseEntity<List<AnnonceFavorisView>> findAnnonceFavorisByIdUser(@PathVariable int idutilisateur,HttpServletRequest request){
+        MyToken token=myTokenService.getToken(request);
+        if(token!=null){
+            return ResponseEntity.ok(annonceFavorisViewService.findAnnonceFavorisByIdUser(idutilisateur));
+        }
+
+        // Retourner une réponse avec un statut non autorisé (401) en cas de token non valide
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
     }
 }
--- src/main/java/com/voiture/voiture/controller/FavorisController.java
+++ src/main/java/com/voiture/voiture/controller/FavorisController.java
@@ -1,24 +1,38 @@
 package com.voiture.voiture.controller;
 
 import com.voiture.voiture.modele.AnnonceFavoris;
+import com.voiture.voiture.modele.MyToken;
 import com.voiture.voiture.service.FavorisService;
+import com.voiture.voiture.service.MyTokenService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/Favoris")
 @CrossOrigin
 public class FavorisController {
     private final FavorisService favorisService;
+    private final MyTokenService myTokenService;
 
-    public FavorisController(FavorisService favorisService) {
+    public FavorisController(FavorisService favorisService,MyTokenService myTokenService) {
         this.favorisService = favorisService;
+        this.myTokenService = myTokenService;
     }
 
     @PostMapping("/create")
-    public AnnonceFavoris create(@RequestBody AnnonceFavoris annonceFavoris){
-        return favorisService.insertFavoris(annonceFavoris);
+    public ResponseEntity<AnnonceFavoris> create(@RequestBody AnnonceFavoris annonceFavoris,HttpServletRequest request){
+        MyToken token=myTokenService.getToken(request);
+        if(token!=null){
+            return ResponseEntity.ok(favorisService.insertFavoris(annonceFavoris));
+        }
+        // Retourner une réponse avec un statut non autorisé (401) en cas de token non valide
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
     }
 
     @GetMapping("/read")
